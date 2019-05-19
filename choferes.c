17f96c80d5e03044d50fb264f30d4600eea700ae
eliday15 <eliasdayanc@gmail.com>
2019-05-18 23:29:09 -0500

    se puede choferes en archivos

--- choferes.c
+++ choferes.c
@@ -37,6 +37,20 @@ void agregarChoferes(Chofer choferes[3], int *contadorC)
   printf("Apellido Materno: %s\n", choferes[*contadorC].apellidoM);
   printf("Estatus: %d\n", choferes[*contadorC].estatus);
 
+  FILE *archivo;
+
+  archivo = fopen("choferes.txt","at");
+
+  fprintf(archivo, "Numero de identificacion: %d\n", choferes[*contadorC].numeroId);
+  fprintf(archivo, "Nombre: %s\n", choferes[*contadorC].nombre);
+  fprintf(archivo, "Apellido Paterno: %s\n", choferes[*contadorC].apellidoP);
+  fprintf(archivo, "Apellido Materno: %s\n", choferes[*contadorC].apellidoM);
+  fprintf(archivo, "Estatus: %d\n", choferes[*contadorC].estatus);
+  fprintf(archivo, "%d\n\n", *contadorC);
+
+  fclose(archivo);
+
+
   (*contadorC)++;
 
   return;
@@ -44,6 +58,7 @@ void agregarChoferes(Chofer choferes[3], int *contadorC)
 void modificarChoferes(Chofer choferes[3], int contadorC)
 {
   int opcion;
+  int i;
   printf("Porfavor ingresa el numero de chofer que se desea modificar\n");
   scanf("%d", &opcion);
   opcion--;
@@ -66,6 +81,19 @@ void modificarChoferes(Chofer choferes[3], int contadorC)
     printf("Apellido Materno: %s\n", choferes[opcion].apellidoM);
     printf("Estatus: %d\n", choferes[opcion].estatus);
 
+    FILE *archivo;
+
+    archivo = fopen("choferes.txt","wt");
+
+    for (i = 0; i < contadorC; i++)
+    {
+      fprintf(archivo, "Numero de identificacion: %d\n", choferes[i].numeroId);
+      fprintf(archivo, "Nombre: %s\n", choferes[i].nombre);
+      fprintf(archivo, "Apellido Paterno: %s\n", choferes[i].apellidoP);
+      fprintf(archivo, "Apellido Materno: %s\n", choferes[i].apellidoM);
+      fprintf(archivo, "Estatus: %d\n", choferes[i].estatus);
+    }
+    fclose(archivo);
 
     return;
   }
